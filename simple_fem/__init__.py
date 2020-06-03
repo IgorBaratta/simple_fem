1b4e035c4658cb29165443f6ab7e3bac122a9157
IgorBaratta <igorbaratta@gmail.com>
2020-06-03 11:03:45 -0300

    fix importing

--- simple_fem/__init__.py
+++ simple_fem/__init__.py
@@ -2,3 +2,6 @@ from .fem import DofMap, Q1Element
 from .function_space import FunctionSpace
 from .mesh import Mesh, ReferenceQuadrilateral
 from .plot import plot
+
+from .quadrature import Quadrature
+from .assemble import assemble_vector, assemble_matrix, apply_bc
