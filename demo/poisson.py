dffe53076760bade2dd5a2f7c541075fcd4fadff
IgorBaratta <igorbaratta@gmail.com>
2020-06-03 12:36:20 -0300

    flake8 fix

--- demo/poisson.py
+++ demo/poisson.py
@@ -4,12 +4,11 @@ from scipy.sparse.linalg import spsolve
 from simple_fem import Mesh, FunctionSpace, Q1Element, plot
 from simple_fem.assemble import assemble_matrix, assemble_vector, apply_bc
 
-
 mesh = Mesh(20, 20)
 element = Q1Element()
 Q = FunctionSpace(mesh, element)
 
-f = lambda x : 4*(-x[1]**2 + x[1])*numpy.sin(numpy.pi*x[0])
+f = lambda x: 4 * (-x[1] ** 2 + x[1]) * numpy.sin(numpy.pi * x[0])
 
 A = assemble_matrix(Q, matrix_type="stiffness")
 b = assemble_vector(Q, f)
